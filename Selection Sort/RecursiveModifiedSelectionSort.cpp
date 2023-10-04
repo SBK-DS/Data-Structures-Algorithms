ff6e3a68d0992fc0349ab3433675fd72ec7676ff
SBK-DS <89160790+SBK-DS@users.noreply.github.com>
2023-10-04 13:02:34 +0500

    Create RecursiveModifiedSelectionSort.cpp

---
+++ Selection Sort/RecursiveModifiedSelectionSort.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int *ptr1, int *ptr2)
+{
+    int temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+}
+
+int FindMinIndex(int arr[], int i, int size, int *steps) 
+{
+    int min=i;
+        
+    for(int j=i; j<size; j++)
+    {
+        if(arr[min]>arr[j])
+        {
+            min = j; 
+            ++*steps;
+        }
+    }
+    
+    return min;
+}
+
+int FindMaxIndex(int arr[], int i, int size, int *steps) 
+{
+    int max=i;
+        
+    for(int j=i; j<size; j++)
+    {
+        if(arr[max]<arr[j])
+        {
+            max = j; 
+            ++*steps;
+        }
+    }
+    
+    return max;
+}
+
+// T(n) = O(n^2) --> Average Case
+// T(n) = O(n^2) --> Best Case
+void SelectionSort(int arr[], int i, int size, int *steps)
+{
+        if( i >= size )
+        {
+            return;
+        }
+        
+        int min = FindMinIndex(arr, i, size, steps);
+        int max = FindMaxIndex(arr, i, size, steps);
+        swap(arr[i], arr[min]);
+        swap(arr[size-1], arr[max]);
+        SelectionSort(arr, ++i, --size, steps);
+}
+
+int main()
+{
+    int arr[10] = {1, 12, 90, 4, 9, 5, 19, 15, 18, 21};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout << "Size is : " << size << endl;
+    cout<<"Hello World" << endl;
+    int steps = 0;
+    SelectionSort(arr, 0, size, &steps);
+    cout << endl;
+    for(int i=0; i<size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl << "steps: " << steps;
+    return 0;
+}
